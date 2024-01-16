893e98d5f2713977858281d486e1117c085f5501
Liz Daly <50752284+lookupdaily@users.noreply.github.com>
2024-01-16 16:09:47 +0000

    Add expandable menu for mobile screens
    This changes the way the menu is displayed on mobile screens. A 'hamburger' menu button is displayed in the mobile navigation which shows or hides the site navigation.
    
    Currently using google material icon font, as shows an icon based on an appropriate keyword.
    
    We will need a JavaScript fallback route, which could either be to show the menu as currently in the dxw playbook, or to use a checkbox toggle functionality rather than a button - as this does not rely on JavaScript.

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,7 +1,14 @@
-function toggleAriaExpanded(e) {
+function toggleAriaExpanded(e, closeText, showText, controlledElementId = undefined) {
   const button = e.target
   const isExpanded = button.getAttribute('aria-expanded') === 'false' ? true : false
+
+  // toggle class name for unrelated elements as we cannot use the aria-expanded state
+  const element = document.getElementById(controlledElementId)
+  if (element) {
+    element.classList.toggle("expanded");
+  }
+
   
   button.setAttribute('aria-expanded', isExpanded)
-  button.children[0].innerText = isExpanded ? 'Hide' : 'Show'
+  button.children[0].innerText = isExpanded ? closeText : showText
 }
